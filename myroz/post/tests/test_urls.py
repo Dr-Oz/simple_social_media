13293f1299f6c2d8a73c549caa7f4cd54b3237d9
Egor <egor@Egors-Air.Dlink>
2023-02-26 19:18:58 +0300

    tests_view

--- myroz/post/tests/test_urls.py
+++ myroz/post/tests/test_urls.py
@@ -1,14 +1,50 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 
+from simple_social_media.myroz.post.models import Post
+User = get_user_model()
 
-class StaticURLTests(TestCase):
+class TaskURLTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Создадим запись в БД для проверки доступности адреса group/test-slug/
+        Post.objects.create(
+            id='post-id',
+            text='Тестовый пост',
+            author='username',
+            group='test-slug',
+        )
+
     def setUp(self):
-        # Устанавливаем данные для тестирования
-        # Создаём экземпляр клиента. Он неавторизован.
+        # Создаем неавторизованный клиент
         self.guest_client = Client()
+        # Создаем пользователя
+        self.user = User.objects.create_user(username='HasNoName')
+        # Создаем второй клиент
+        self.authorized_client = Client()
+        # Авторизуем пользователя
+        self.authorized_client.force_login(self.user)
 
     def test_homepage(self):
         # Отправляем запрос через client,
         # созданный в setUp()
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_group_slug(self):
+        """Страница /group/<slug>/ доступна любому пользователю."""
+        response = self.guest_client.get('/group/test-slug/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_page(self):
+        """Страница /profile/<username>/ доступна любому пользователю."""
+        response = self.guest_client.get('/profile/username')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_page(self):
+        """Страница /post/<post_id>/ доступна любому пользователю."""
+        response = self.guest_client.get('/post/post-id')
+        self.assertEqual(response.status_code, 200)
+
+
